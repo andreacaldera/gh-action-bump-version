e0b48e3371d9d06ab250e8568badf99be563526b
Andrea Caldera <andrea.caldera@gmail.com>
2021-09-19 21:36:14 +0100

    test

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ Toolkit.run(async (tools) => {
 
   fs.writeFileSync('package.json', JSON.stringify({ ...pkg, version: latestTag }, null, 2));
   await runCommand(`git diff`);
-  const commits = await runCommand(`git log origin/main...$(git branch --show-current) --oneline --grep=major`);
+  const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1];
+  console.log(`Current branch ${currentBranch} ${process.env.GITHUB_REF}`);
+  const commits = await runCommand(`git log origin/main...${currentBranch} --oneline --grep=major`);
   if (commits.includes('major')) {
     console.log('Major release');
   } else if (commits.includes('minor')) {
